ff500b81a66e062112fe5c3220b0afc856465f17
Sreevani <vani.lanka@gmail.com>
2026-01-08 16:56:44 +0530

    second commit

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 from flask import Flask, render_template, request
 import pickle
 import numpy as np
+import os
 app = Flask(__name__)
 model = pickle.load(open("model.pkl", "rb"))
 @app.route('/')
@@ -17,3 +18,5 @@ def predict():
     return render_template("index.html", prediction_text=result)
 if __name__ == "__main__":
     app.run(debug=True)
+    port=int(os.environ.get("PORT",5000)) 
+    app.run(host="0.0.0.0",port=port)
