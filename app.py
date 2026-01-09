9eb0b8c441c2c3cc50ef10482ed6476d24ec5814
Sreevani <vani.lanka@gmail.com>
2026-01-09 17:41:21 +0530

    third commit

--- app.py
+++ app.py
@@ -17,6 +17,5 @@ def predict():
     result = "Heart Disease Detected " if prediction == 1 else "No Heart Disease "
     return render_template("index.html", prediction_text=result)
 if __name__ == "__main__":
-    app.run(debug=True)
     port=int(os.environ.get("PORT",5000)) 
     app.run(host="0.0.0.0",port=port)
